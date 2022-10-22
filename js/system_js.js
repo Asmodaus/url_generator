688dddd65fecb022e434d8e90e86820db5a22860
Your Name <you@example.com>
2022-10-22 13:35:13 +0300

    fx

--- js/system_js.js
+++ js/system_js.js
@@ -39,11 +39,32 @@
 		copyText.setSelectionRange(0, 99999); // For mobile devices
 	  
 		 // Copy the text inside the text field
-		navigator.clipboard.writeText(copyText.value).then(function() { // Копируем текст в буффер обмена
+		
+
+		if (navigator.clipboard && window.isSecureContext) {
+			// navigator clipboard api method'
+			navigator.clipboard.writeText(copyText.value).then(function() { // Копируем текст в буффер обмена
 			console.log(copyText.value);
 					}, function() {
 						console.log(copyText);
 					});
+		} else {
+			// text area method
+			let textArea = document.createElement("textarea");
+			textArea.value = copyText.value;
+			// make the textarea out of viewport
+			textArea.style.position = "fixed";
+			textArea.style.left = "-999999px";
+			textArea.style.top = "-999999px";
+			document.body.appendChild(textArea);
+			textArea.focus();
+			textArea.select();
+			return new Promise((res, rej) => {
+				// here the magic happens
+				document.execCommand('copy') ? res() : rej();
+				textArea.remove();
+			});
+		}
 				
 	   
 	  }
